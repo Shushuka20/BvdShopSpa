ea0c1fc80302681580d3f42b0fd6354e938417b6
Ilya Klepov <ilyaklepov@gmail.com>
2018-04-15 05:20:11 +0300

    Update model.repository.ts

--- src/app/admin/repositories/model.repository.ts
+++ src/app/admin/repositories/model.repository.ts
@@ -1,9 +1,7 @@
-export interface IRepository<T> {
+export interface IRepository<T> { 
     entities: T[];
 
     getEntities(): T[];
     
-    saveEntity(entity: T): void;
-
-    
+    saveEntity(entity: T): void;   
 }
